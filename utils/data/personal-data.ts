84f8e0f2b39418c4207847f7b68144d07aa82fad
Amit Yadav <samkr8991@gmail.com>
2026-03-03 04:53:35 +0530

    REMOVED UNWANERED

--- utils/data/personal-data.ts
+++ utils/data/personal-data.ts
@@ -23,15 +23,9 @@ export const personalData: PersonalDataType = {
   description:
     "Full Stack Developer (MERN) with 4 years of experience. Proficient in Node.js, Nest.js, Express.js, MongoDB, React.js, TypeScript, and AWS. Experienced in building scalable SaaS platforms, third-party API integrations, and end-to-end feature development across domains like e-commerce, finance, and media. Skilled in backend architecture, secure RESTful APIs, and cross-functional collaboration.",
   email: "amitkryadav8991@gmail.com",
-  phone: "",
   address: "Bengaluru, Karnataka",
   github: "https://github.com/amit1github",
-  facebook: "",
   linkedIn: "https://www.linkedin.com/in/amit-kr-yadav/",
-  twitter: "",
-  stackOverflow: "",
-  leetcode: "",
-  devUsername: "",
   resume:
     "https://drive.google.com/file/d/1wUBDUf2IjTkoZyF_vRXwtKNSG310jquY/view?usp=sharing",
 };
